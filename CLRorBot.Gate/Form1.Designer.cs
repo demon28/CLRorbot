77dd9207762535c80c2d583b538ddd1162053e85
demon28 <274733956@qq.com>
2018-07-31 18:59:49 +0800

    完成

--- CLRorBot.Gate/Form1.Designer.cs
+++ CLRorBot.Gate/Form1.Designer.cs
@@ -320,8 +320,6 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Checked = true;
-            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
             this.checkBox1.Location = new System.Drawing.Point(626, 356);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(66, 16);
